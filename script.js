408793803c555bb55a64864f27bf1c9f7a508fd5
noumanmani <168138758+noumanmani@users.noreply.github.com>
2024-06-24 02:20:13 +0500

    complete project

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSidebar(){
+    const side =document.querySelector(".side-bar")
+    side.style.display="flex" 
+ }
+function hidesidebar(){
+    const side =document.querySelector(".side-bar")
+    side.style.display="none" 
+}
